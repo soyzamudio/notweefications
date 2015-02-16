df3017260ea7f7400cdd3ebf3dcf2a6f3c0673ac
Jose Zamudio <zamudio@outlook.com>
2015-02-16 11:44:52 -0800

    Store tweets in mongodb

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 var Twitter = require('twitter');
+var MongoClient = require('mongodb').MongoClient
+, assert = require('assert');
+
+// Connection URL
+var url = 'mongodb://localhost:27017/myproject';
 
 var client = new Twitter({
   consumer_key: process.env.TWITTER_CONSUMER_KEY,
@@ -7,11 +12,28 @@ var client = new Twitter({
   access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 });
 
-client.stream('statuses/filter', {track: 'coding house,codinghouse'}, function(stream) {
+client.stream('statuses/filter', {track: 'coding,bugs,coding house,codinghouse'}, function(stream) {
   stream.on('data', function(tweet) {
-    console.log(tweet.text);
+    // Use connect method to connect to the Server
+    MongoClient.connect(url, function(err, db) {
+      assert.equal(null, err);
+      console.log("Connected correctly to server");
+
+
+      // Get the documents collection
+      var collection = db.collection('documents');
+      // Insert some documents
+      collection.insert([
+        tweet
+      ], function(err, result) {
+        assert.equal(err, null);
+        console.log("Inserted 3 documents into the document collection");
+        db.close();
+      });
+    });
   });
   stream.on('error', function(error) {
     throw error;
   });
 });
+
