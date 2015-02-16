e9b2ba908ed6bd8c0176d0e456d2e34067430980
Jose Zamudio <zamudio@outlook.com>
2015-02-16 10:21:32 -0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+var Twitter = require('twitter');
+
+var client = new Twitter({
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+});
+
+client.stream('statuses/filter', {track: 'coding house,codinghouse'}, function(stream) {
+  stream.on('data', function(tweet) {
+    console.log(tweet.text);
+  });
+  stream.on('error', function(error) {
+    throw error;
+  });
+});
